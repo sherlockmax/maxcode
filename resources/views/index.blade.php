708ad77a3b22cd19dc0c15177331f6f642f894c0
max_huang <max_huang@notReal.com>
2016-08-23 16:39:47 +0800

    finished UI

---
+++ resources/views/index.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Information
+                </div>
+
+                <div class="panel-body">
+
+                    <div>
+                        <label class="col-xs-5 control-label">No.</label>
+                        <label class="control-label">201608220001</label>
+                    </div>
+
+                    <div>
+                        <label class="col-xs-5 control-label">Create Time.</label>
+                        <label class="control-label">{{Date("Y-m-d H:i:s")}}</label>
+                    </div>
+
+                    <div>
+                        <label class="col-xs-5 control-label">Round.</label>
+                        <label class="control-label">1/3</label>
+                    </div>
+
+                    <div>
+                        <label class="col-xs-5 control-label">Start Time.</label>
+                        <label class="control-label">2016-08-22 13:00:00</label>
+                    </div>
+
+                    <div>
+                        <label class="col-xs-5 control-label">End Time.</label>
+                        <label class="control-label">2016-08-22 13:01:00</label>
+                    </div>
+
+                    <div>
+                        <label class="col-xs-5 control-label">Round Code.</label>
+                        <label class="control-label">2/?/?</label>
+                    </div>
+
+                    <div>
+                        <label class="col-xs-5 control-label">Final Code.</label>
+                        <label class="control-label">20</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Status
+                </div>
+
+                <div class="panel-body">
+                    <div style="text-align: center">Loading...</div>
+                </div>
+            </div>
+        </div>
+
+`       <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Controller
+                </div>
+
+                <div class="panel-body" style="text-align: center;">
+                    <form action="" method="POST" class="form-horizontal">
+                        <div class="form-group">
+                            <div>
+                                <label class="control-label">Method 1 : Choose Odd or Even</label>
+                            </div>
+                            <div id="numTypeController">
+                                <label style="width: 200px; margin-right: 5px" class="btn btn-default">
+                                    <input type="radio" name="numType" id="num_odd" value="odd"><i class="fa fa-btn fa-venus"></i>Odd
+                                </label>
+                                <label style="width: 200px" class="btn btn-default">
+                                    <input type="radio" name="numType" id="num_even" value="even"><i class="fa fa-btn fa-venus-double"></i>Even
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div>
+                                <label class="control-label">Method 2 : Choose a number</label>
+                            </div>
+                            <div id="numbersController">
+                                @for ($i = 1; $i <= 40; $i++)
+                                    <label style="width: 55px;margin-top: 3px" class="btn btn-default">
+                                        <input type="radio" name="numbers" id="num_{{$i}}" value="{{$i}}">
+                                        @if($i < 10)
+                                            {{"0$i"}}
+                                        @else
+                                            {{$i}}
+                                        @endif
+                                    </label>
+                                @endfor
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button style="width: 200px; margin-right: 5px;" type="button" id="btn_reset" class="btn btn-warning">
+                                <i class="fa fa-btn fa-refresh"></i>Reset
+                            </button>
+                            <button style="width: 200px" type="button" id="btn_reset" class="btn btn-success">
+                                <i class="fa fa-btn fa-paper-plane"></i>Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Final Code</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+    <!-- Styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+        .fa-btn {
+            margin-right: 6px;
+        }
+        .btn-default:checked{
+            background-color: red !important;
+        }
+        input[type=radio]{
+            display: none;
+        }
+        .active{
+            background-color: slategray !important;
+            color: #FFFF00 !important;
+        }
+    </style>
+    <script>
+        $(document).ready(function(){
+            $('input[type=radio]').click(function(){
+                var element_name = $(this).attr("name");
+                var radioId = "num_" + $(this).val();
+
+                //reset all radio (unchecked)
+                $('input[name='+element_name+']').attr('checked', false);
+                //reset all label (removeClass "active")
+                $('#'+element_name+'Controller').find("label").removeClass("active");
+
+                $('#' + radioId).attr('checked', true);
+                $('#' + radioId).parent("label").addClass("active");
+            });
+
+            $('#btn_reset').click(function(){
+                //reset all radio (unchecked)
+                $('input[type=radio]').attr('checked', false);
+                //reset all label (removeClass "active")
+                $('div[id*=Controller]').find("label").removeClass("active");
+            });
+        });
+
+    </script>
+</head>
+<body id="app-layout">
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Final Code
+            </a>
+        </div>
+
+    </div>
+</nav>
+
+@yield('content')
+</body>
+</html>
