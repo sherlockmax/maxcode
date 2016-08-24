3050852c6e081acfce712097358ba075ff823460
max_huang <max_huang@notReal.com>
2016-08-24 09:30:00 +0800

    create js/css file and inclue

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by max on 16年8月24日.
+ */
+$(document).ready(function(){
+    $('input[type=radio]').click(function(){
+        var element_name = $(this).attr("name");
+        var radioId = "num_" + $(this).val();
+
+        //reset all radio (unchecked)
+        $('input[name='+element_name+']').attr('checked', false);
+        //reset all label (removeClass "active")
+        $('#'+element_name+'Controller').find("label").removeClass("active");
+
+        $('#' + radioId).attr('checked', true);
+        $('#' + radioId).parent("label").addClass("active");
+    });
+
+    $('#btn_reset').click(function(){
+        //reset all radio (unchecked)
+        $('input[type=radio]').attr('checked', false);
+        //reset all label (removeClass "active")
+        $('div[id*=Controller]').find("label").removeClass("active");
+    });
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <title>Final Code</title>
     <link rel="icon" href="favicon.png" type="image/x-icon" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="{{ URL::asset('js/main.js') }}"></script>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
@@ -15,51 +16,7 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-    <style>
-        body {
-            padding: 0px;
-            margin: 0 auto;
-            font-family: 'Lato';
-        }
-        .fa-btn {
-            margin-right: 6px;
-        }
-        .btn-default:checked{
-            background-color: red !important;
-        }
-        input[type=radio]{
-            display: none;
-        }
-        .active{
-            background-color: slategray !important;
-            color: #FFFF00 !important;
-        }
-    </style>
-    <script>
-        $(document).ready(function(){
-            $('input[type=radio]').click(function(){
-                var element_name = $(this).attr("name");
-                var radioId = "num_" + $(this).val();
-
-                //reset all radio (unchecked)
-                $('input[name='+element_name+']').attr('checked', false);
-                //reset all label (removeClass "active")
-                $('#'+element_name+'Controller').find("label").removeClass("active");
-
-                $('#' + radioId).attr('checked', true);
-                $('#' + radioId).parent("label").addClass("active");
-            });
-
-            $('#btn_reset').click(function(){
-                //reset all radio (unchecked)
-                $('input[type=radio]').attr('checked', false);
-                //reset all label (removeClass "active")
-                $('div[id*=Controller]').find("label").removeClass("active");
-            });
-        });
-
-    </script>
+    <link href="{{ URL::asset('css/main.css') }}" rel="stylesheet">
 </head>
 <body id="app-layout">
 <nav class="navbar navbar-default">
