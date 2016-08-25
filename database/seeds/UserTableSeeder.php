4f1f6386056b33e852ac1a97250a250667d18eca
max_huang <max_huang@notReal.com>
2016-08-25 09:35:09 +0800

    create UserTableSeeder to add test user account

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: max
- * Date: 16年8月25日
- * Time: 上午9:19
- */
+
+use Illuminate\Database\Seeder;
+use \App\User;
+
+class UserTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('users')->delete();
+        User::create([
+            'account' => 'finalgod',
+            'name'    => 'Final God',
+            'password' => Hash::make('123456'),
+            'cash' => 10000,
+        ]);
+    }
+}
