4160eb17b17a65376ec846ea47bd02b212acff72
max_huang <max_huang@notReal.com>
2016-09-06 03:15:58 -0400

    update UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -153,7 +153,7 @@
                                     @else
                                         <div class="col-xs-12 col-xs-offset-2">
                                             <button type="button" id="btn_reset" class="col-xs-4 btn btn-warning">
-                                                <i class="fa fa-btn fa-refresh"></i>重新填寫
+                                                <i class="fa fa-btn fa-refresh"></i>清除選擇
                                             </button>
                                             <button type="submit" id="btn_submit" class="col-xs-4 btn btn-success">
                                                 <i class="fa fa-btn fa-paper-plane"></i>確認下注
@@ -172,7 +172,7 @@
                         本期下注資訊&nbsp;-&nbsp;玩法&nbsp;1
                     </div>
 
-                    <div id="bet_history_box" class="panel-body">
+                    <div id="bet_history_box" class="panel-body" style="max-height: 355gipx; overflow: auto">
                         您尚未對本期遊戲進行下注
                     </div>
                 </div>
@@ -182,7 +182,7 @@
                         本期下注資訊&nbsp;-&nbsp;玩法&nbsp;2
                     </div>
 
-                    <div id="bet_history_box" class="panel-body">
+                    <div id="bet_history_box" class="panel-body" style="max-height: 355px; overflow: auto">
                         您尚未對本期遊戲進行下注
                     </div>
                 </div>
